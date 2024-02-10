8bac6ffc50cfa06c87fb7738a7f9316a02bb9117
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-02-10 14:37:29 -0500

    completed lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode='w', encoding='utf-8') as log_file:
+        log_file.write(f'{file_content}')
 
+
+    
 def append_file(file_name, append_content):
-    pass
+
+    with open(f'{file_name}.txt', mode='a', encoding='utf-8') as log_file:
+         log_file.write(f'{append_content}')
+
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', mode='r', encoding='utf-8') as log_file:
+        file_content = log_file.read()
+    return file_content
